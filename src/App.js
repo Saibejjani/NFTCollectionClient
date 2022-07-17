d501dc9a3919ae9cd2d1440c79b487b1c71d7f3b
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2022-07-17 14:54:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,314 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import NFTGen from "./utils/NFTGen.json";
+import NFTCol from "./utils/NFTCol.json";
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="input-group py-3 "
+  />
+);
 
-function App() {
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x000c3b547eA7d55e861A586eaa74c46A4df6CeE7";
+  const [formData, setFormData] = useState({
+    Name: "",
+    Symbol: "",
+    URI: "",
+    MintAmount: "",
+  });
+  const [fetched, setFetched] = useState(0);
+
+  const [prev, setPrev] = useState();
+  const [totalCollections, setTotalColletions] = useState([]);
+  const [currentContractAddress, setCurrentContractsAddress] = useState([]);
+  const [currentColletion, setCurrentCollection] = useState([]);
+
+  const handleChange = (e, name) => {
+    setFormData((prevstate) => ({ ...prevstate, [name]: e.target.value }));
+    console.log(formData);
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts) {
+          setCurrentAccount(accounts[0]);
+        } else {
+          console.log("no authorized account found");
+        }
+      } else {
+        console.log("Ethereum object not found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setCurrentAccount(accounts[0]);
+
+        console.log(accounts[0]);
+      } else {
+        alert("Please install metamaks");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchColletions = async () => {
+    console.log(currentColletion);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          contractAddress,
+          NFTGen,
+          signer
+        );
+
+        const len = await connectedContract.collectionsLength();
+
+        for (let i = 0; i < len.toNumber() - fetched; i++) {
+          const currentAddress = await connectedContract.collections(i);
+          setCurrentContractsAddress((current) => [...current, currentAddress]);
+          const currentContract = new ethers.Contract(
+            currentAddress,
+            NFTCol,
+            signer
+          );
+          let uri = await currentContract.tokenURI(1);
+          uri = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}`;
+          const tokenMetadata = await fetch(uri).then((response) =>
+            response.json()
+          );
+          let { image } = tokenMetadata;
+
+          image = `https://ipfs.io/ipfs/${image.split("ipfs://")}`;
+          image = image.replace(",", "");
+          const name = await currentContract.name();
+          const symbol = await currentContract.symbol();
+
+          setTotalColletions((current) => [
+            ...current,
+            { image, name, symbol },
+          ]);
+
+          console.log(totalCollections);
+
+          console.log(tokenMetadata);
+        }
+
+        setFetched(len.toNumber());
+        console.log(currentContractAddress);
+        console.log(len.toNumber());
+      } else {
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchThisCollection = async (index) => {
+    console.log(currentColletion);
+
+    try {
+      const { ethereum } = window;
+      console.log(prev);
+      if (prev !== index) {
+        setPrev(index);
+        console.log(prev);
+        setCurrentCollection([]);
+      }
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const currentContract = new ethers.Contract(
+          currentContractAddress[index],
+          NFTCol,
+          signer
+        );
+        console.log(currentContractAddress[index]);
+        const totalSupply = await currentContract.totalSupply();
+
+        for (let i = 1; i <= totalSupply.toNumber(); i++) {
+          let uri = await currentContract.tokenURI(i);
+          uri = `https://ipfs.io/ipfs/${uri.split("ipfs://")[1]}`;
+          const tokenMetadata = await fetch(uri).then((response) =>
+            response.json()
+          );
+          let { image, name, description } = tokenMetadata;
+
+          image = `https://ipfs.io/ipfs/${image.split("ipfs://")}`;
+          image = image.replace(",", "");
+          setCurrentCollection((current) => [
+            ...current,
+            { name, description, image },
+          ]);
+          console.log(image);
+        }
+      } else {
+        console.log("ethereum object not found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const MintCollection = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          contractAddress,
+          NFTGen,
+          signer
+        );
+        const { Name, Symbol, URI, MintAmount } = formData;
+
+        const rec = await connectedContract.createNewCollection(
+          Name,
+          Symbol,
+          URI,
+          MintAmount
+        );
+        const trx = await rec.wait();
+        console.log(trx);
+        fetchColletions();
+      } else {
+        console.log("Ethereum object not found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+        {currentAccount ? (
+          <h6 className="my-5">{currentAccount}</h6>
+        ) : (
+          <button className="btn btn-outline-primary" onClick={connectWallet}>
+            ConnectWallet
+          </button>
+        )}
+
+        <div className="front">
+          <Input
+            placeholder="TEST COLLECTION"
+            name="Name"
+            type="text"
+            handleChange={handleChange}
+          />
+          <br />
+          <Input
+            placeholder="TSTC"
+            name="Symbol"
+            type="text"
+            handleChange={handleChange}
+          />
+          <br />
+          <Input
+            placeholder="ipfs://your-uri/"
+            name="URI"
+            type="text"
+            handleChange={handleChange}
+          />
+          <br />
+          <Input
+            placeholder="20"
+            name="MintAmount"
+            type="number"
+            handleChange={handleChange}
+          />
+          <br />
+          <button className="btn btn-primary" onClick={MintCollection}>
+            MintCollection
+          </button>
+
+          <button
+            onClick={() => {
+              fetchColletions();
+            }}
+            className="btn btn-primary mx-3"
+          >
+            fetch collections
+          </button>
+        </div>
+        <br />
+
+        <div className="my-5">
+          <h1>Colletions</h1>
+          <div className="row row-col-1 row-cols-md-3 mb-3 text-center my-5">
+            {totalCollections.map((ele, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col"
+                  onClick={() => {
+                    fetchThisCollection(index);
+                  }}
+                >
+                  <div className="card mb-4 rounded shadow-sm">
+                    <img src={ele.image} style={{ margin: "20px" }} alt="nft" />
+                    <div className="header">
+                      <h4>{ele.name}</h4>
+                      <h6>{ele.symbol}</h6>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <br />
+        <div>
+          <h1 className="text my-5 mx-5"> NFTS</h1>
+          <div className="row row-col-1 row-cols-md-3 mb-3 text-center">
+            {currentColletion.map((ele, index) => {
+              return (
+                <div className="col" key={index}>
+                  <div className="card mb-4 rounded shadow-sm">
+                    <img src={ele.image} style={{ margin: "20px" }} alt="nft" />
+                    <div className="header">
+                      <h4>{ele.name}</h4>
+                      <h6>{ele.description}</h6>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
